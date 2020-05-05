5a9124892ec9eff689975ef3fda779e0f1a88206
masaun <masanoriuno@gmail.com>
2020-05-05 16:48:59 +0900

    Execute _createToken() in frontend

--- client/src/components/StakeholderRegistry/index.js
+++ client/src/components/StakeholderRegistry/index.js
@@ -29,11 +29,23 @@ export default class StakeholderRegistry extends Component {
             route: window.location.pathname.replace("/", "")
         };
 
+        this.createToken = this.createToken.bind(this);
         this._balanceOfContract = this._balanceOfContract.bind(this);
     }
 
+    createToken = async () => {
+        const { accounts, web3, dai, stakeholder_registry, token_factory } = this.state;
 
-    
+        const _tokenName = "UMA Synthetic Gold May 2020";
+        const _tokenSymbol = "UMAGold_May2020";
+        const _tokenDecimals = 18;
+
+        let res1 = await token_factory.methods.createToken(_tokenName, _tokenSymbol, _tokenDecimals).send({ from: accounts[0] });
+        console.log('=== response of _createToken() function 1 ===\n', res1);
+
+        let res2 = await stakeholder_registry.methods._createToken(_tokenName, _tokenSymbol, _tokenDecimals).send({ from: accounts[0] });
+        console.log('=== response of _createToken() function 2 ===\n', res2);
+    }
 
     _balanceOfContract = async () => {
         const { accounts, web3, dai, stakeholder_registry } = this.state;
@@ -72,10 +84,11 @@ export default class StakeholderRegistry extends Component {
      
         let StakeholderRegistry = {};
         let Dai = {};
+        let TokenFactory = {};
         try {
           StakeholderRegistry = require("../../../../build/contracts/StakeholderRegistry.json");  // Load artifact-file of StakeholderRegistry
           Dai = require("../../../../build/contracts/IERC20.json");    //@dev - DAI
-          //Dai = require("../../../../build/contracts/Dai.json");  //@dev - DAI
+          TokenFactory = require("../../../../build/contracts/TokenFactory.json");  //@dev - TokenFactory.sol
         } catch (e) {
           console.log(e);
         }
@@ -127,6 +140,15 @@ export default class StakeholderRegistry extends Component {
             );
             console.log('=== instanceDai ===', instanceDai);
 
+            //@dev - Create instance of TokenFactory.sol
+            let instanceTokenFactory = null;
+            let TOKEN_FACTORY_ADDRESS = tokenAddressList["Kovan"]["UMAGold_May2020"]; //@dev - UMAGold_May2020（on Kovan）
+            instanceTokenFactory = new web3.eth.Contract(
+              TokenFactory.abi,
+              DAI_ADDRESS,
+            );
+            console.log('=== instanceTokenFactory ===', instanceTokenFactory);
+
 
             if (StakeholderRegistry) {
               // Set web3, accounts, and contract to the state, and then proceed with an
@@ -142,8 +164,10 @@ export default class StakeholderRegistry extends Component {
                 isMetaMask, 
                 stakeholder_registry: instanceStakeholderRegistry,
                 dai: instanceDai,
+                token_factory: instanceTokenFactory,
                 STAKEHOLDER_REGISTRY_ADDRESS: STAKEHOLDER_REGISTRY_ADDRESS,
                 DAI_ADDRESS: DAI_ADDRESS,
+                TOKEN_FACTORY_ADDRESS: TOKEN_FACTORY_ADDRESS
               }, () => {
                 this.refreshValues(
                   instanceStakeholderRegistry
@@ -182,6 +206,8 @@ export default class StakeholderRegistry extends Component {
                               borderColor={"#E8E8E8"}
                         >
                             <h4>UMA Synthetic Tokens HackMoney</h4> <br />
+
+                            <Button size={'small'} mt={3} mb={2} onClick={this.createToken}> Create Synthetic Token </Button> <br />
 
                             <Button mainColor="DarkCyan" size={'small'} mt={3} mb={2} onClick={this._balanceOfContract}> Balance of contract </Button> <br />
                         </Card>

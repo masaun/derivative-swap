c51ff3006eee92f137b1db117be11bcb7d2725bd
masaun <masanoriuno@gmail.com>
2020-05-05 18:54:27 +0900

    Add 3 contractAddress ('ExpiringMultiPartyCreator.sol', 'IdentifierWhitelist.sol', 'Registry.sol') to contractAddress.js

--- migrations/contractAddress/contractAddress.js
+++ migrations/contractAddress/contractAddress.js
@@ -2,6 +2,9 @@ module.exports = {
     "Kovan": {
         "UMA": {
             "TokenFactory": "0x478049C316035a3Cf0e1d73fdeD5BC45D1CeFde4",  // TokenFactory.sol from UMA
+            "ExpiringMultiPartyCreator": "0x606232b0Ee7364B63bf90F8dc847AF9572eFcbA0",
+            "IdentifierWhitelist": "0x851588DD61B11D52180563cfCEeF9a87Db5D7c69",
+            "Registry": "0x519D00b1a6d56205D3d13a4d28aDC82CC849d0E4"
         }
     },
     "Rinkeby": {

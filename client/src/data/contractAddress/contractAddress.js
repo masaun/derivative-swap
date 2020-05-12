125e5ecb95061cc9084fa4df3549913b1a859a02
masaun <masanoriuno@gmail.com>
2020-05-12 09:47:18 +0900

    Add 'Finder' to contractAddress.js

--- client/src/data/contractAddress/contractAddress.js
+++ client/src/data/contractAddress/contractAddress.js
@@ -6,7 +6,8 @@ export const contractAddressList = {
             "ExpiringMultiPartyCreator": "0x606232b0Ee7364B63bf90F8dc847AF9572eFcbA0",
             "IdentifierWhitelist": "0x851588DD61B11D52180563cfCEeF9a87Db5D7c69",
             "Registry": "0x519D00b1a6d56205D3d13a4d28aDC82CC849d0E4",
-            "AddressWhitelist": "0xAc803f66CB647999036fC6fACd205c3a00650b0b"
+            "AddressWhitelist": "0xAc803f66CB647999036fC6fACd205c3a00650b0b",
+            "Finder": "0x6850e2B45D2951301a0BFED44F0a80E9F32fAfEe",
         }
     }
 }
--- migrations/contractAddress/contractAddress.js
+++ migrations/contractAddress/contractAddress.js
@@ -5,7 +5,8 @@ module.exports = {
             "TokenFactory": "0x478049C316035a3Cf0e1d73fdeD5BC45D1CeFde4",  // TokenFactory.sol from UMA
             "ExpiringMultiPartyCreator": "0x606232b0Ee7364B63bf90F8dc847AF9572eFcbA0",
             "IdentifierWhitelist": "0x851588DD61B11D52180563cfCEeF9a87Db5D7c69",
-            "Registry": "0x519D00b1a6d56205D3d13a4d28aDC82CC849d0E4"
+            "Registry": "0x519D00b1a6d56205D3d13a4d28aDC82CC849d0E4",
+            "Finder": "0x6850e2B45D2951301a0BFED44F0a80E9F32fAfEe",
         }
     },
     "Rinkeby": {

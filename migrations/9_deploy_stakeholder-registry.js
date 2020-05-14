a03cdfa560a8625affab39d2df003eea98ba5749
masaun <masanoriuno@gmail.com>
2020-05-14 12:53:59 +0900

    Remove extra codes

--- migrations/9_deploy_stakeholder-registry.js
+++ migrations/9_deploy_stakeholder-registry.js
@@ -1,8 +1,6 @@
 var StakeholderRegistry = artifacts.require("StakeholderRegistry");
 var CreateContractViaNew = artifacts.require("CreateContractViaNew");
-//var ExpiringMultiPartyLib = artifacts.require("ExpiringMultiPartyLib");
 var ExpiringMultiPartyCreator = artifacts.require("ExpiringMultiPartyCreator");
-//var IdentifierWhitelist = artifacts.require("IdentifierWhitelist");
 var IERC20 = artifacts.require("IERC20");
 
 //@dev - Import from exported file
@@ -12,15 +10,8 @@ var walletAddressList = require('./walletAddress/walletAddress.js');
 
 const _erc20 = tokenAddressList["Kovan"]["DAI"];                            // DAI address on Kovan
 const _createContractViaNew = CreateContractViaNew.address;
-//const _expiringMultiPartyLib = contractAddressList["Kovan"]["UMA"]["ExpiringMultiPartyLib"];
 const _expiringMultiPartyCreator = ExpiringMultiPartyCreator.address;
 const _addressWhitelist = contractAddressList["Kovan"]["UMA"]["AddressWhitelist"];
-//const _finder = contractAddressList["Kovan"]["UMA"]["Finder"];
-//const _tokenFactory = contractAddressList["Kovan"]["UMA"]["TokenFactory"];
-//const _identifierWhitelist = IdentifierWhitelist.address;
-
-//@dev - Create a instance of ExpiringMultiPartyLib.sol
-//const expiringMultiPartyLib = ExpiringMultiPartyLib.at(_expiringMultiPartyLib);
 
 const depositedAmount = web3.utils.toWei("0.1");    // 2.1 DAI which is deposited in deployed contract. 
 
@@ -32,12 +23,7 @@ module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(StakeholderRegistry,
                           _erc20,
                           _createContractViaNew, 
-                          //_expiringMultiPartyLib,
-                          _expiringMultiPartyCreator,
-                          //_addressWhitelist
-                          // _finder,
-                          // _tokenFactory
-                          //_identifierWhitelist
+                          _expiringMultiPartyCreator
           ).then(async function(stakeholderRegistry) {
         if(ownerAddress && ownerAddress!="") {
             console.log(`=== Transfering ownerhip to address ${ownerAddress} ===`)

912c1b08b0240af9f428a5e44935cb4344a9bef5
masaun <masanoriuno@gmail.com>
2020-05-15 09:51:08 +0900

    Execute addMember() for collateralToken address

--- migrations/9_deploy_stakeholder-registry.js
+++ migrations/9_deploy_stakeholder-registry.js
@@ -9,7 +9,7 @@ var tokenAddressList = require('./tokenAddress/tokenAddress.js');
 var contractAddressList = require('./contractAddress/contractAddress.js');
 var walletAddressList = require('./walletAddress/walletAddress.js');
 
-const _erc20 = tokenAddressList["Kovan"]["DAI"];                            // DAI address on Kovan
+const _collateralAddress = tokenAddressList["Kovan"]["DAI"];                            // DAI address on Kovan
 const _createContractViaNew = CreateContractViaNew.address;
 const _registry = Registry.address;
 const _expiringMultiPartyCreator = ExpiringMultiPartyCreator.address;
@@ -18,15 +18,20 @@ const depositedAmount = web3.utils.toWei("0.1");    // 2.1 DAI which is deposite
 
 
 module.exports = async function(deployer, network, accounts) {
+    // Deployer address
+    const deployerAddress = accounts[0];
+
     // Initialize owner address if you want to transfer ownership of contract to some other address
     let ownerAddress = walletAddressList["WalletAddress1"];
 
     // Add Role to EMPCreator contractAddress
-    //const registry = await Registry.at(_registry);
-    //await registry.addMember(1, _expiringMultiPartyCreator);  //@dev - 1 is "Owner" Role
+    const registry = await Registry.at(_registry);
+    await registry.addMember(1, _expiringMultiPartyCreator, { from: deployerAddress });  //@dev - 1 is "Owner" Role
+    await registry.addMember(1, _collateralAddress, { from: deployerAddress });          //@dev - 1 is "Owner" Role
+
 
     await deployer.deploy(StakeholderRegistry,
-                          _erc20,
+                          _collateralAddress,  // ERC20 address
                           _createContractViaNew, 
                           _expiringMultiPartyCreator,
                           _registry
@@ -39,7 +44,7 @@ module.exports = async function(deployer, network, accounts) {
 
     const stakeholderRegistry = await StakeholderRegistry.deployed();
 
-    const iERC20 = await IERC20.at(_erc20);
+    const iERC20 = await IERC20.at(_collateralAddress);  // _collateralAddress is ERC20 address
 
     //@dev - Transfer 2.1 DAI from deployer's address to contract address in advance
     await iERC20.transfer(stakeholderRegistry.address, depositedAmount);

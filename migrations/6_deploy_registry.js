5c134484f7df3bc490eb6687af99e7bc36c299e8
masaun <masanoriuno@gmail.com>
2020-05-16 13:49:14 +0900

    Add changeImplementationAddress() of Finder.sol into migration-file

--- migrations/6_deploy_registry.js
+++ migrations/6_deploy_registry.js
@@ -1,7 +1,16 @@
 const Registry = artifacts.require("Registry");
+const Finder = artifacts.require("Finder");
+const { interfaceName } = require("../utils/Constants.js");
+
 
 module.exports = async function(deployer, network, accounts) {
     const deployerAddress = accounts[0];
 
-    await deployer.deploy(Registry, { from: deployerAddress });
+  const { contract: registry } = await deploy(Registry, { from: deployerAddress });
+
+    const finder = await Finder.deployed();
+    await finder.changeImplementationAddress(web3.utils.utf8ToHex(interfaceName.Registry), registry.address, {
+        from: deployerAddress
+    });
+
 };
